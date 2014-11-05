c795f3aa7faef8033e91a29fa8aa242efee56b9a
Niels Søholm <nm@9la.dk>
2014-11-05 15:22:30 +0800

    Added startup animation to percentage bar

--- js/main.js
+++ js/main.js
@@ -127,22 +127,71 @@ d3.xml("img/index.svg", "image/svg+xml", function(xml) {
             .attr("width", gW) // max 540
             .style("fill", "#d3d2d2")
             .attr("transform", "translate(127,1024)")
-            .attr("height", 22);
+            .attr("height", 22)
+            .attr("id", "passedPercRect");
 
         d3.select("#g3083").insert("rect", ":first-child")
             .attr("width", bW) // max 540
             .style("fill", "#58b4f2")
             .attr("transform", "translate(" + (127+gW) + ",1024)")
-            .attr("height", 22.1);
+            .attr("height", 22.1)
+            .attr("id", "remainPercRect");
 
-        var percF = Math.round(perc * 100)/100;
-        d3.select("#tspan3077").text(percF * 100 + "%");
-        d3.select("#passedPercLabel").text((1-percF) * 100 + "%");
-        d3.select("#remainPercLabel").text(percF * 100 + "%");
+        var percF = Math.round(perc * 100);
+        var percR = 100 - percF;
+        d3.select("#tspan3077")
+            .datum({value: perc})
+            .text(percF + "%");
+        d3.select("#passedPercLabel")
+            .datum({value: perc})
+            .text(percF + "%");
+        d3.select("#remainPercLabel")
+            .datum({value: 1-perc})
+            .text(percR + "%");
+    };
+
+    // Interpolated animation function to adjust percentage labels gradually
+    var labelTween = function(transition, newVal) {
+        transition.tween("text", function(data) {
+            var interpolate = d3.interpolate(data.value, newVal);
+            return function(t) {
+                data.value = interpolate(t);
+                console.log(data.value);
+                this.textContent = Math.round(data.value * 100) + "%";
+            };
+        });
     };
 
     var adjustPercentage = function(perc) {
-        // TODO: Implement adjustment with animation (IDEA: animate labels as well)
+        var gW = 540 * (1-perc);
+        var bW = 540 * perc;
+
+        d3.select("#passedPercRect")
+            .transition()
+            .duration(2000)
+            .attr("width", gW);
+
+        d3.select("#remainPercRect")
+            .transition()
+            .duration(2000)
+            .attr("width", bW)
+            .attr("transform", "translate(" + (127+gW) + ",1024)");
+
+        // Animate text labels
+        d3.select("#passedPercLabel")
+            .transition()
+            .duration(2000)
+            .call(labelTween, 1 - perc);
+
+        d3.select("#remainPercLabel")
+            .transition()
+            .duration(2000)
+            .call(labelTween, perc);
+
+        d3.select("#tspan3077")
+            .transition()
+            .duration(2000)
+            .call(labelTween, perc);
     };
 
     // Initialize all 3 counters
@@ -155,7 +204,8 @@ d3.xml("img/index.svg", "image/svg+xml", function(xml) {
     var periodDur = moment.utc(arrival).diff(start);
     var remDur = moment.utc(arrival).diff(moment());
     var remPerc = remDur / periodDur;
-    initializePercentage(remPerc);
+    initializePercentage(1.0);
+    adjustPercentage(remPerc);
 
     // Start interval timer to adjust counters once every second
     setInterval(function () {
